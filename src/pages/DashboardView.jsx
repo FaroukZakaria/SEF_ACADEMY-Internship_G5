437b7421c149691920292ea0c78387742eb4e5e1
belhadjt <belhadjt@gmail.com>
2026-07-06 16:05:15 +0100

    feat:dasboard view h w

--- src/pages/DashboardView.jsx
+++ src/pages/DashboardView.jsx
@@ -10,17 +10,17 @@ export default function DashboardView() {
         
             <div className='flex flex-col gap-6 p-4 lg:p-8 w-full h-auto'>
 
-                <div className="space-y-6">
+                <div className="space-y-6 w-full h-auto">
                     <StatsGrid />
                 </div>
 
-                <div className=" grid gap-4 xl:grid-cols-[1.1fr_0.9fr]">
+                <div className=" grid gap-4 xl:grid-cols-[1.1fr_0.9fr] w-full h-auto">
                     <OrderStatusCard />
                     <TopProductsCard />
                 </div>
 
 
-                <div>
+                <div className='w-full h-auto'>
                     <RecentOrdersTable />
                 </div>
                 
